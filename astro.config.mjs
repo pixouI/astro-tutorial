6ee8bab4411754ca2c833d601c3986f43a2f0f4a
pixoul <pixoul@pixoul.archive>
2024-09-25 20:46:10 +0200

    actual finishing touches. dark/light mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://pixoul.netlify.app"
+  site: "https://pixoul.netlify.app",
+  integrations: [preact()]
 });
